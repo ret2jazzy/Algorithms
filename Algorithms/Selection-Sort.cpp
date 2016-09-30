0ddb4a6c6d631653bdb3edb647c5adab3d85409f
Drigg3r <drigg3r@yandex.com>
2016-09-30 12:59:19 -0500

    Create Selection-Sort.cpp

---
+++ Algorithms/Selection-Sort.cpp
@@ -0,0 +1,39 @@
+/*This just a simpe selection-sort of array(Vector is better i know) made just for fun*/
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void sortar(int arr[], int size)
+{
+    int swapped = 1;
+    while(swapped){
+        swapped = 0;
+        for(int i  = 0; i < size -1; ++i){
+            if(arr[i] > arr[i+1]){
+                swap(arr[i], arr[i+1]);
+                swapped = 1;
+            }
+        }
+        
+    }
+}
+
+int main()
+{
+    cout << "Size of array " << endl;
+    int s;cin >> s;
+    int arraya[s];
+    for(int i = 0; i < s; ++i){
+        int x;
+        cin >> x;
+        arraya[i] = x;
+    }
+    sortar(arraya, s);
+    cout << endl;
+    for(int i = 0; i < s; ++i){
+        cout << arraya[i] << endl;
+    }
+}
